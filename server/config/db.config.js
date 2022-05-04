1254cf3c8a3b70f35b3de9a1d6f2a23b05788efd
shyamalaG-shyam <shyamalagopal719@gmail.com>
2022-05-04 13:11:34 +0530

    removed server

--- server/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    port:process.env.DB_PORT,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    dialect:process.env.DIALECT
-}
--- server/routes/admin.routes.js
+++
@@ -1,5 +0,0 @@
-const adminController=require('../controller/admin.controller')
-const router=require('express').Router()
-router.post('/addAdmin',adminController.addAdmin)
-
-module.exports=router
